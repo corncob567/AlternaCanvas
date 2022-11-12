4707f0e302681e538a21eda9eb1affc968bef188
Daniel Hackney <corncob567dh@gmail.com>
2022-11-12 02:06:19 -0500

    Added default route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Syllabus from './components/pages/syllabus/Syllabus';
 import Assignments from './components/pages/assignments/assignments';
 import Modules from './components/pages/modules/modules';
@@ -14,27 +14,28 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-        <Route path="/compg" element={<App/>} >
-          <Route exact path="syllabus" element={<Syllabus courseId={'compg'}/>}/>
-          <Route path="assignments" element={<Assignments courseId={'compg'}/>}/>
-          <Route path="modules" element={<Modules courseId={'compg'}/>}/>
-          <Route path="announcements" element={<Announcements courseId={'compg'}/>}/>
-          <Route path="grades" element={<Grades courseId={'compg'}/>}/>
-        </Route>
-        <Route path="/ui" element={<App/>} >
-          <Route exact path="syllabus" element={<Syllabus courseId={'ui'}/>}/>
-          <Route path="assignments" element={<Assignments courseId={'ui'}/>}/>
-          <Route path="modules" element={<Modules courseId={'ui'}/>}/>
-          <Route path="announcements" element={<Announcements courseId={'ui'}/>}/>
-          <Route path="grades" element={<Grades courseId={'ui'}/>}/>
-        </Route>
-        <Route path="/seniordesign" element={<App/>} >
-          <Route exact path="syllabus" element={<Syllabus courseId={'seniordesign'}/>}/>
-          <Route path="assignments" element={<Assignments courseId={'seniordesign'}/>}/>
-          <Route path="modules" element={<Modules courseId={'seniordesign'}/>}/>
-          <Route path="announcements" element={<Announcements courseId={'seniordesign'}/>}/>
-          <Route path="grades" element={<Grades courseId={'seniordesign'}/>}/>
-        </Route>
+      <Route path="/" element={<Navigate replace to="/ui" />} />
+      <Route path="/compg" element={<App/>} >
+        <Route exact path="syllabus" element={<Syllabus courseId={'compg'}/>}/>
+        <Route path="assignments" element={<Assignments courseId={'compg'}/>}/>
+        <Route path="modules" element={<Modules courseId={'compg'}/>}/>
+        <Route path="announcements" element={<Announcements courseId={'compg'}/>}/>
+        <Route path="grades" element={<Grades courseId={'compg'}/>}/>
+      </Route>
+      <Route path="/ui" element={<App/>} >
+        <Route exact path="syllabus" element={<Syllabus courseId={'ui'}/>}/>
+        <Route path="assignments" element={<Assignments courseId={'ui'}/>}/>
+        <Route path="modules" element={<Modules courseId={'ui'}/>}/>
+        <Route path="announcements" element={<Announcements courseId={'ui'}/>}/>
+        <Route path="grades" element={<Grades courseId={'ui'}/>}/>
+      </Route>
+      <Route path="/seniordesign" element={<App/>} >
+        <Route exact path="syllabus" element={<Syllabus courseId={'seniordesign'}/>}/>
+        <Route path="assignments" element={<Assignments courseId={'seniordesign'}/>}/>
+        <Route path="modules" element={<Modules courseId={'seniordesign'}/>}/>
+        <Route path="announcements" element={<Announcements courseId={'seniordesign'}/>}/>
+        <Route path="grades" element={<Grades courseId={'seniordesign'}/>}/>
+      </Route>
     </Routes>
   </BrowserRouter>
 );
