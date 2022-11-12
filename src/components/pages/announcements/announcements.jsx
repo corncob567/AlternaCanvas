7bd6ea845f1e61196db876790d23260aa3f60cf4
Daniel Hackney <corncob567dh@gmail.com>
2022-11-12 12:17:46 -0500

    Pretty icons

--- src/components/pages/announcements/announcements.jsx
+++ src/components/pages/announcements/announcements.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import AnnouncementList from './announcementlist.jsx';
 import '../../../styles/main.scss';
+import { FaBullhorn } from 'react-icons/fa';
 
 const Announcements = (props) => {
     const courseId = props.courseId
     return (
         <div className='pageWrapper'>
-            <h1 className='pageTitle'>Announcements</h1>
+            <h1 className='pageTitle'>Announcements <FaBullhorn/></h1>
             <AnnouncementList courseId={courseId}/>
         </div>
     );
