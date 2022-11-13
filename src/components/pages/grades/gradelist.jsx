bd8e732e5536b65061b8ce8bdc6d6c2d3954d3b4
alexjmills <alexandriajmills@yahoo.com>
2022-11-13 13:15:20 -0500

    dummy data

--- src/components/pages/grades/gradelist.jsx
+++ src/components/pages/grades/gradelist.jsx
@@ -5,64 +5,64 @@ import '../../../styles/main.scss';
 const grades = [
     {
         date: "11/10/22",
-        title: "Project 1",
-        earnedPoints: 11,
+        title: "Project 2",
+        earnedPoints: 48,
+        totalPoints: 50,
+        courseId: "ui"
+    },
+    {
+        date: "11/09/22",
+        title: "Homework 5",
+        earnedPoints: 15,
         totalPoints: 20,
         courseId: "ui"
     },
     {
-        date: "11/10/22",
-        title: "Project 1",
-        earnedPoints: 11,
+        date: "10/30/22",
+        title: "Homework 4",
+        earnedPoints: 18,
         totalPoints: 20,
         courseId: "ui"
     },
     {
-        date: "11/10/22",
+        date: "10/15/22",
         title: "Project 1",
-        earnedPoints: 11,
-        totalPoints: 20,
+        earnedPoints: 45,
+        totalPoints: 50,
         courseId: "ui"
     },
     {
         date: "11/10/22",
-        title: "Project 1",
-        earnedPoints: 11,
+        title: "Project 3",
+        earnedPoints: 14,
         totalPoints: 20,
-        courseId: "ui"
+        courseId: "compg"
     },
     {
         date: "11/10/22",
-        title: "Project 1",
-        earnedPoints: 11,
+        title: "Project 2",
+        earnedPoints: 12,
         totalPoints: 20,
         courseId: "compg"
     },
     {
         date: "11/10/22",
         title: "Project 1",
-        earnedPoints: 11,
+        earnedPoints: 16,
         totalPoints: 20,
         courseId: "compg"
     },
     {
         date: "11/10/22",
-        title: "Project 1",
-        earnedPoints: 11,
-        totalPoints: 20,
-        courseId: "compg"
-    },
-    {
-        date: "11/10/22",
-        title: "Project 1",
-        earnedPoints: 11,
+        title: "Check-point 3",
+        earnedPoints: 14,
         totalPoints: 20,
         courseId: "seniordesign"
     },
     {
         date: "11/10/22",
-        title: "Project 1",
-        earnedPoints: 11,
+        title: "Presentation",
+        earnedPoints: 17,
         totalPoints: 20,
         courseId: "seniordesign"
     },
