88661daaa23a227efa5bb13bce80d097d18150bc
Baru <bhargav.yogesh@gmail.com>
2022-11-14 16:42:54 -0500

    chat button on assignments placed

--- src/components/pages/assignments/assignment.jsx
+++ src/components/pages/assignments/assignment.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import '../../../styles/main.scss';
+import AssignmentChat from './AssignmentChat';
 
 const Assignment = ({ assignmentID, assignmentName, dueDate, dueTime }) => {
     
     return (
         <div className='assignment'>
-            <h2 style={{ textDecoration: 'none' }}>{assignmentName}</h2>
-            <p style={{ fontStyle: 'italic', textDecoration: 'none' }}>{dueDate} @ {dueTime}</p>
+            <div>
+                <h2 style={{ textDecoration: 'none' }}>{assignmentName}</h2>
+                <p style={{ fontStyle: 'italic', textDecoration: 'none' }}>{dueDate} @ {dueTime}</p> 
+            </div>
+
+            <AssignmentChat></AssignmentChat>
         </div>
     );
 };
