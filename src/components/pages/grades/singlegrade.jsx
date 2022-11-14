81f7cf9bdac1fadce9793061e85cd57a42787c0d
alexjmills <alexandriajmills@yahoo.com>
2022-11-13 23:35:32 -0500

    total weighted class grade

--- src/components/pages/grades/singlegrade.jsx
+++ src/components/pages/grades/singlegrade.jsx
@@ -9,44 +9,26 @@ const SingleGrade = (props) => {
 
     const percentage = earnedPoints/totalPoints;
     let calcGrade = function(){
-        if(courseId == "ui"){
-            if (percentage > .92){
-                return "A";
-            }
-            else if (percentage > .82){
-                return "B";
-            }
-            else if (percentage > .72){
-                return "C";
-            }else if (percentage > .62){
-                return "D";
-            }
-            else{
-                return "F";
-            }
+        if (percentage > .89){
+            return "A";
+        }
+        else if (percentage > .79){
+            return "B";
+        }
+        else if (percentage > .69){
+            return "C";
+        }else if (percentage > .59){
+            return "D";
         }
         else{
-            if (percentage > .9){
-                return "A";
-            }
-            else if (percentage > .8){
-                return "B";
-            }
-            else if (percentage > .7){
-                return "C";
-            }else if (percentage > .6){
-                return "D";
-            }
-            else{
-                return "F";
-            }
+            return "F";
         }
     }
 
     const letterGrade = calcGrade();
     
     return (
-        <div className={`singleAnnouncementWrapper${isAfter ? ' recent' : ''}`}>
+        <div className={`singleGradeWrapper`}>
             <h3 className='announcementName'>{title}</h3>
             <p>{date}</p>
             <label>{letterGrade}</label>
