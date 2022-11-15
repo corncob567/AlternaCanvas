cf0b1f28cd4a51ec60360c02af3a767c0e9ed04c
Baru <bhargav.yogesh@gmail.com>
2022-11-14 20:26:18 -0500

    time

--- src/components/chat/ChatBody.jsx
+++ src/components/chat/ChatBody.jsx
@@ -6,7 +6,7 @@ function ChatBody(props) {
     return (
         <div className='chat-body'>
             {props.messages.data.map((message, index) => {
-                return <ChatMessage key={index} className={message.author === props.user ? 'right-align' : ''}content={message.content} author={message.author} user={props.user}></ChatMessage>
+                return <ChatMessage key={index} className={message.author === props.user ? 'right-align' : ''}content={message.content} author={message.author} time={message.time} user={props.user}></ChatMessage>
             })}
         </div>
         
