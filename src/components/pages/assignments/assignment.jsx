5a818d4c27e340b674dfa81d2a26477f818c1cbf
Baru <bhargav.yogesh@gmail.com>
2022-11-14 20:06:49 -0500

    relative positioning assignment chat

--- src/components/pages/assignments/assignment.jsx
+++ src/components/pages/assignments/assignment.jsx
@@ -9,16 +9,21 @@ const Assignment = ({ assignmentID, assignmentName, dueDate, dueTime, setChatDat
     return (
         <>
         <div className='assignment' onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)}>
-            <h2 style={{ textDecoration: 'none' }}>{assignmentName}</h2>
-            <p style={{ fontStyle: 'italic', textDecoration: 'none' }}>{dueDate} @ {dueTime}</p>
-            {isHover ? 
-                <div className='assignment--hoverMenu'>
-                    <div className='chatBubbleIcon'>
-                        <ChatBubble setChatData={setChatData} user={user} assignmentName={assignmentName}/>
-                    </div>
-                    <NotifBell/>
-                </div> 
-            : ''}
+            <div>
+                <h2 style={{ textDecoration: 'none' }}>{assignmentName}</h2>
+                <p style={{ fontStyle: 'italic', textDecoration: 'none' }}>{dueDate} @ {dueTime}</p>
+            </div>
+            <div style={{ justifySelf: 'right'}}>
+                {isHover ? 
+                    <div className='assignment--hoverMenu'>
+                        <div className='chatBubbleIcon'>
+                            <ChatBubble setChatData={setChatData} user={user} assignmentName={assignmentName}/>
+                        </div>
+                        <NotifBell/>
+                    </div> 
+                : ''}
+            </div>
+            
         </div>
         
         </>
